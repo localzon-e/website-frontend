8026d701a3c9a5828be4e65d2c0de55ce8cf995e
jpkmiller <j.mueller5021@gmail.com>
2021-02-03 21:49:19 +0100

    working on travis

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/website-frontend/'
-        : '/'
+        : '/',
+    chainWebpack: config => {
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .loader('url-loader')
+            .tap(options => Object.assign(options, {limit: 10240}))
+    }
 }
